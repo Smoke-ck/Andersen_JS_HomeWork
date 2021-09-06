1ec452195465e9db44661ab331635a496d80eeb6
Smoke-ck <72155871+Smoke-ck@users.noreply.github.com>
2021-09-06 14:05:58 +0300

    Add files via upload

---
+++ Second home work/script.js
@@ -0,0 +1,68 @@
+const makeObjectDeepCopy = (obj) => {
+
+    let cloneObject = Object.assign({}, obj);
+
+    Object.keys(cloneObject).forEach(key => (
+        cloneObject[key] =
+        typeof obj[key] === "object" ? makeObjectDeepCopy(obj[key]) : obj[key])
+    );
+
+    if (Array.isArray(obj) && obj.length) {
+        return (cloneObject.length = obj.length) && Array.from(cloneObject)
+    } else if (Array.isArray(obj)) {
+        return Array.from(cloneObject)
+    } else {
+        return cloneObject
+    }
+};
+
+
+
+
+function selectFromInterval(array, firstValue, secondValue) {
+
+    if (!Array.isArray(array)) {
+        throw new Error('Not an array ');
+    } else if (!Number.isInteger(firstValue) || !Number.isInteger(secondValue)) {
+        throw new Error('Invalid number');
+    } else if (secondValue < firstValue) {
+        ([firstValue, secondValue] = [secondValue, firstValue])
+    }
+
+    for (i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+            throw new Error('Invalid number');
+        }
+    }
+
+    return array.filter(item => (firstValue <= item && item <= secondValue))
+}
+
+
+
+
+const myIterable = { from: 2, to: 4 };
+
+
+function iterableOfObject(object) {
+    const { from, to } = object;
+
+    if (from > to || isNaN(from) || isNaN(to)) {
+        throw new Error('Error iteration')
+    } else {
+        object[Symbol.iterator] = function () {
+            return {
+                fromValue: this.from,
+                toValue: this.to,
+                next() {
+                    if (this.fromValue <= this.toValue) {
+                        return { done: false, value: this.fromValue++ };
+                    }
+                    else {
+                        return { done: true };
+                    }
+                }
+            };
+        };
+    }
+}
