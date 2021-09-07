372e227d156f883a4dd76fb8ff85d880ccb9ce74
Smoke-ck <72155871+Smoke-ck@users.noreply.github.com>
2021-09-07 10:27:45 +0300

    Update script.js

--- Second home work/script.js
+++ Second home work/script.js
@@ -43,28 +43,29 @@ function selectFromInterval(array, firstValue, secondValue) {
 
 
 
-const myIterable = { 
-from: 2 ,
-to:6,
-[Symbol.iterator] : function () {
-	const { from, to } = myIterable;
-			if (from > to || isNaN(from) || isNaN(to)) {
-     			throw new Error('Error iteration')
-   			 	} 
-            return {
-                fromValue: this.from,
-                toValue: this.to,
-                next() {
-                	if (this.fromValue <= this.toValue) {
-                        return { done: false, value: this.fromValue++ };
-                    }
-                    else {
-                        return { done: true };
-                    }
+
+const myIterable = {
+    from: 2,
+    to: 6,
+    [Symbol.iterator]: function () {
+        const { from, to } = myIterable;
+        if (from > to || isNaN(from) || isNaN(to)) {
+            throw new Error('Error iteration')
+        }
+        return {
+            fromValue: this.from,
+            toValue: this.to,
+            next() {
+                if (this.fromValue <= this.toValue) {
+                    return { done: false, value: this.fromValue++ };
                 }
-            };
-        }};
+                else {
+                    return { done: true };
+                }
+            }
+        };
+    }
+};
 
-for(let item of myIterable){
-
+for (let item of myIterable) {
 }
