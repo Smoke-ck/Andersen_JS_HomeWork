d243f371a353b97c00445672f183dd8e58807c33
Smoke-ck <72155871+Smoke-ck@users.noreply.github.com>
2021-09-08 12:25:32 +0300

    Update script.js

--- Third home work/script.js
+++ Third home work/script.js
@@ -1,25 +1,13 @@
 Array.prototype.myFilter = function (callBack, thisArgs) {
-
-    let context = this;
-    let result = [];
-    let object = Object(this);
-
-    if (arguments.length > 1) {
-        context = thisArgs;
-    }
-    for (let i = 0; i < context.length; i++) {
-        const isAdd = callBack.call(context, context[i], i, object);
-        if (i in object) {
-            if (isAdd) {
-                result = [...result, context[i]]
-            }
+	let result = [];
+	thisArgs = this;
+    thisArgs.forEach( (element) => {
+        if (callBack.call(thisArgs[element], element,thisArgs)) {
+            result = [...result,element]
         }
-    }
-    return result;
+    });  
+  return result;
 }
-
-
-
 
 function createDebounceFunction(callback, timeInMs) {
 
